d29f4f21b28f441ac0f505263a7196a952ac4207
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-01-28 09:41:28 +0100

    Config file + beginning

---
+++ Tracker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by heikko on 28/01/2020.
+//
+
+#include "Tracker.hpp"
---
+++ Tracker.hpp
@@ -0,0 +1,14 @@
+//
+// Created by heikko on 28/01/2020.
+//
+
+#ifndef TRACKER_TRACKER_HPP
+#define TRACKER_TRACKER_HPP
+
+
+class Tracker {
+
+};
+
+
+#endif //TRACKER_TRACKER_HPP
