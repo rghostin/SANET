23443ab4ee81d79191f8fb81c13691c88c753e1a
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-01-30 18:17:29 +0100

    - [POSITION] Fonction générant le string associé à la Position

--- Position.hpp
+++ Position.hpp
@@ -11,4 +11,10 @@ inline void print_position(const Position& pos) {
     printf("Longitude : %f\nLatitude: %f\n\n", pos.longitude, pos.latitude);
 }
 
+
+inline std::string get_positionInfos(const Position& pos) {
+    return "Longitude : " + std::to_string(pos.longitude) + " | Lattitude: " + std::to_string(pos.latitude);
+}
+
+
 #endif
