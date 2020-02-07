72c95380a0a00f110f5ce07c177f717a34cdcb11
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-02-07 14:37:11 +0100

    TODO :   - [ABSTRACTBROADCASTNODE] void*

--- AbstractBroadcastNode.hpp
+++ AbstractBroadcastNode.hpp
@@ -105,7 +105,7 @@ void AbstractBroadcastNode<P>::_setup_socket_bind() {
         LOG_F(WARNING, "ARM architecture detected");
         memset(&b_iface, 0, sizeof(b_iface));
         snprintf(b_iface.ifr_name, sizeof(b_iface.ifr_name), b_iface_name);
-        if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, (void)*&b_iface, sizeof(b_iface)) < 0) {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, (void*)&b_iface, sizeof(b_iface)) < 0) {
             close(sockfd);
             perror("setsockopt (SO_BINDTODEVICE)");
             throw;
