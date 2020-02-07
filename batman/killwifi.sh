6a1bbbf47a60d2b47f610e62d2129034ebc13c30
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-02-07 19:22:24 +0100

    TODO :   - [KILLWIFI] flush

--- batman/killwifi.sh
+++ batman/killwifi.sh
@@ -27,10 +27,12 @@ fi
 dhclient -r "$W_IFACE"
 killall -9 dhclient || echo "No process dhclient to kill"
 ip link set "$W_IFACE" down
+ip addr flush "$W_IFACE"
 killall -9 wpa_supplicant || echo "No process wpa_supplicant to kill"
 
 # kill batman if exists
 check_interface_exists "$B_IFACE"
 if [[ $? -eq 0 ]]; then
   ip link set dev "$B_IFACE" down
+  ip addr flush "$B_IFACE"
 fi
