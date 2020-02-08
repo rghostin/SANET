d0a2c30d2892f66bb7eb91fa2d2488288040e369
GHOSTIN  Rawad <rghostin@di-pc77.ulb.ac.be>
2020-02-08 12:49:57 +0100

    run nodeid param

--- batman/run.sh
+++ batman/run.sh
@@ -7,5 +7,17 @@ set -e
 
 source "$GLOBALS_SCRIPT"
 
-"$BATMANIFY_SCRIPT"
-"$ROBIN_PROG"
+# force run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# check number of params
+if [[ $# -ne 1 ]]; then
+   echo "Error: Invalid number of arguments"
+   exit 1
+fi
+
+"$BATMANIFY_SCRIPT" "$1"
+"$ROBIN_PROG" -i "$1"
