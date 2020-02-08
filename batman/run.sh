17dae15840158974101f4a47f528331b13ff7d64
GHOSTIN  Rawad <rghostin@ulb.ac.be>
2020-02-08 13:16:31 +0100

    getopts run

--- batman/run.sh
+++ batman/run.sh
@@ -7,6 +7,9 @@ set -e
 
 source "$GLOBALS_SCRIPT"
 
+function usage() { echo "Usage: $0 [-i <0-254>] [-v <-2-9>]" 1>&2; exit 1; }
+
+
 # force run as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
@@ -19,7 +22,29 @@ if [[ $# -ne 1 ]]; then
    exit 1
 fi
 
-cd "$ROOT"
-make rebuild
+while getopts ":i:v:" o; do
+    case "${o}" in
+        i)
+            i=${OPTARG}
+            (( s >= 0 && s <= 254 )) || usage
+            ;;
+        v)
+            v=${OPTARG}
+            (( v >= -2 && v <= 9 )) || usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${i}" ] || [ -z "${v}" ]; then
+    usage
+fi
+
+echo "i = ${s}"
+echo "v = ${p}"
+
 "$BATMANIFY_SCRIPT" "$1"
 "$ROBIN_PROG" -i "$1"
