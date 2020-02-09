4a8ee4d8bb75e85dfe5ef4bbd7361bdc5aebaacc
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-02-09 11:29:02 +0100

    PULL-REQUEST :   - git ignore + robin.cpp + settings.hpp

--- settings.hpp
+++ settings.hpp
@@ -2,14 +2,14 @@
 #define _SETTINGS_HPP_
 
 // Global settings
-#define CFG_NODEID_FNAME "nodeid.cfg"
+#define CFG_NODEID "nodeid.cfg"
 
 // Batman specific settings
 #define BATMAN_IFACE "bat0"
 
 // Tracking settings
 #define TRACKING_SERVER_PORT 5820
-#define TRACKING_HEARTBEAT_PERIOD 10
+#define TRACKING_HEARTBEAT_PERIOD 30
 #define TRACKING_PEER_LOSS_TIMEOUT 4 * TRACKING_HEARTBEAT_PERIOD
 #define TRACKING_PERIOD_CHECK_NODEMAP TRACKING_HEARTBEAT_PERIOD
 
