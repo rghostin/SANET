2d9c0b905f76a9a3e6cff0a17d2143cfe23487bf
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-02-15 12:02:17 +0100

    Fix bash scripts + Cmake

--- batman/setup.sh
+++ batman/setup.sh
@@ -19,24 +19,28 @@ function changeKeyboard {
 function usage() {
 	echo "This script must be run with super-user privileges.
 	Usage: $0 [arguments]
-	  - i:NodeID (0 <= i <= 254)
+	- i:NodeID (0 <= i <= 254)
     - h:Hostname
     - a:Antenna (0 : to enable | 1 : to disable)"
+    exit 1
 }
 
 cd "$ROOT"
 
 # force run as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
 # check params
-if [[ $# != 2 ]]; then
+if [[ $# -lt 2 ]]; then
   echo "[!] Error usage: setup.sh hostid hostname"
   exit 1
 fi
+
+#Antenna_off
+a=1
 
 # getting params
 while getopts ":h:i:a:" o; do
@@ -48,15 +52,22 @@ while getopts ":h:i:a:" o; do
         h)
             h=${OPTARG}
             ;;
-        a)
-            a=${OPTARG}
-            ;;
+        #a)
+        #    a=${OPTARG} || usage
+        #    ;;
         *)
             usage
             ;;
     esac
 done
 shift $((OPTIND-1))
+
+if [ -z "${i}" ] || [ -z "${a}" ]; then
+    usage
+fi
+
+echo "i = ${i}"
+echo "v = ${a}"
 
 echo "changing hostname to $h"
 hostnamectl set-hostname "$h"
@@ -94,20 +105,30 @@ if [[ $a -eq 0 ]] ; then  # Enabling Antenna
     echo "- setting static IP address=$P_IP_ADDR/24"
     ip addr add dev "$P_IFACE" "$P_IP_ADDR"/24
 
-    echo '[*] Writing dhcpd configuration'
-    #dhcpd_file="/etc/dhcp/dhcpd.conf"  # Todo cp file
+    echo '[*] Overwriting dhcpd configuration'
+    mv "$RSRC_DIR/dhcpd.conf" "/etc/dhcp/dhcpd.conf"
 
-    # Todo cp file -> /etc/default/isc-dhcp-server
+    echo '[*] Overwriting isc-dhcp-server default configuration'
+    mv "$RSRC_DIR/isc-dhcp-server" "/etc/default/isc-dhcp-server"
 
     echo '[*] Restarting DHCP server'
     service isc-dhcp-server restart
 
-    # Todo cp file -> /etc/hostapd/hostapd.conf + /etc/default/hostapd
+    echo '[*] Overwriting hostapd configuration'
+    mv "$RSRC_DIR/hostapd.conf" "/etc/hostapd/hostapd.conf"
+
+    echo '[*] Overwriting hostapd default configuration'
+    mv "$RSRC_DIR/hostapd" "/etc/default/hostapd"
+
+    echo '[*] Overwriting sysctl configuration'
+    mv "$RSRC_DIR/sysctl.conf" "/etc/sysctl.conf"
 
     echo '[*] Activation of forwarding'
     sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 else
+    systemctl stop isc-dhcp-server
     systemctl mask isc-dhcp-server
+    systemctl stop hostapd
     systemctl mask hostapd
 fi
 
