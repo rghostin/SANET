820a914fd0608ea638a67946d624c04eaa4fe11e
heikko <hugo.lloreda.sanchez@ulb.ac.be>
2020-02-17 10:53:12 +0100

    cmake + ccobin before new branch

---
+++ CCobin.cpp
@@ -0,0 +1,75 @@
+#include <thread>
+#include <atomic>
+#include <csignal>
+#include <iostream>
+#include "loguru.hpp"
+#include "utils.hpp"
+#include "settings.hpp"
+#include "common.hpp"
+#include "CCServer.hpp"
+
+
+// Stopping mechanism
+std::atomic<bool> process_stop(false);
+
+void exit_handler(int s) {
+    LOG_F(WARNING, "Caught signal %d", s);
+    process_stop = true;
+    LOG_F(INFO, "program_stop=true");
+}
+
+
+int main(int argc, char** argv) {
+
+    const uint8_t nodeID = read_int_from_file(CFG_NODEID);
+    int opt;
+    char *checkLong;
+    long int verbose_value;
+
+    // force running as root on ARM
+#ifdef __aarch64__
+    auto user_running = getuid();
+        if (user_running != 0) {
+            perror("Robin must be run as root");
+            throw;
+        }
+#endif
+
+    if (argc < 2) {
+        std::cerr << "Not enough arguments" << std::endl;
+        throw;
+    }
+
+    LOG_F(WARNING, "NodeID=%d", nodeID);
+
+    // setup logs
+    loguru::g_stderr_verbosity = loguru::Verbosity_INFO;
+    loguru::add_file("robin.log", loguru::Truncate, loguru::Verbosity_3);
+    loguru::set_thread_name("robin_main");
+    while((opt = getopt(argc, argv, "i:v:")) != -1)
+    {
+        switch(opt)
+        {
+            case 'v':
+                verbose_value = strtol (optarg, &checkLong, 10);
+                if (std::strcmp(checkLong, "\0") == 0 and (-2 <= verbose_value) and (verbose_value <= 9)) {
+                    loguru::g_stderr_verbosity = static_cast<int>(verbose_value);
+                    LOG_F(WARNING, "Logging changed to the value -> {%d} through parameter -v", loguru::g_stderr_verbosity);
+                }
+                break;
+        }
+    }
+
+    // setup signals
+    signal(SIGINT, exit_handler);
+
+    // Tracking server
+    CCServer ccserver(CC_SERVER_PORT, nodeID);
+    ccserver.start();
+
+    // join all services
+    ccserver.join();
+
+    LOG_F(WARNING, "CCobin exiting");
+    return 0;
+}
