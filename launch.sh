20c88903edca778bd88bbe74b13b1f46e21c51b0
hlloreda <32882487+hlloreda@users.noreply.github.com>
2020-02-23 12:04:11 +0100

    tmux doesn't like to sleep

--- launch.sh
+++ launch.sh
@@ -30,6 +30,6 @@ tmux new-session -d bash
 tmux split-window -h bash
 #sends keys to first and second terminals
 tmux send -t 0:0.0 "python3 flightplanning/flight_server.py" C-m
-sleep 15s
+sleep 5s
 tmux send -t 0:0.1 "./robin -v 3" C-m
 tmux -2 attach-session -d
