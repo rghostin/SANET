c9b3e5b900b6dea0554edd9b510f719549b4a922
hlloreda <32882487+hlloreda@users.noreply.github.com>
2020-02-23 14:02:30 +0100

    fix send

--- launch.sh
+++ launch.sh
@@ -35,9 +35,9 @@ function tmux_start() {
     tmux new-session -d bash
     tmux split-window -h bash
     #sends keys to first and second terminals
-    tmux send -t 0:0.0 "ping google.com" C-m
+    tmux send -t 0:0.0 "python3 flightplanning/flight_server.py" C-m
     sleep 5s
-    tmux send -t 0:0.1 "ping google.be" C-m
+    tmux send -t 0:0.1 "./robin -v 3" C-m
 }
 
 tmuxproc="$(pgrep tmux)"
