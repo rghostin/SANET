19a69423e15ce41803924cdca8e6a1975aa12e95
hlloreda <32882487+hlloreda@users.noreply.github.com>
2020-02-23 13:59:04 +0100

    launch in new terminal

--- launch.sh
+++ launch.sh
@@ -18,18 +18,38 @@ sqlite3 "$DB_PATH" "$query"
 
 
 # tmux
+trap end_servers SIGINT
+
+function end_servers() {
+        echo "[*] Trapped CTRL-C"
+        tmux kill-session -t 0
+        tmuxproc="$(pgrep tmux)"
+        if [ -n "$tmuxproc" ] ; then 
+            echo "Killing last tmux with PID : $tmuxproc"
+            kill "$tmuxproc"
+        fi
+        exit
+}
+
+function tmux_start() {
+    tmux new-session -d bash
+    tmux split-window -h bash
+    #sends keys to first and second terminals
+    tmux send -t 0:0.0 "ping google.com" C-m
+    sleep 5s
+    tmux send -t 0:0.1 "ping google.be" C-m
+}
+
 tmuxproc="$(pgrep tmux)"
 echo "Tmux running with PID : $tmuxproc"
 pkill -f tmux
-tmuxproc="$(pgrep tmux)"
 if [ -n "$tmuxproc" ] ; then 
     echo "Killing last tmux with PID : $tmuxproc"
     kill "$tmuxproc"
 fi
-tmux new-session -d bash
-tmux split-window -h bash
-#sends keys to first and second terminals
-tmux send -t 0:0.0 "python3 flightplanning/flight_server.py" C-m
-sleep 5s
-tmux send -t 0:0.1 "./robin -v 3" C-m
-tmux -2 attach-session -d
+
+tmux_start &
+
+mate-terminal --command "tmux attach-session;"  # Only works on NO PC
+
+cat
