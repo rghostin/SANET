0269b26e31145b8c948b756cb98e4bf08a9b626e
rghostin <rghostin@ulb.ac.be>
2020-03-11 17:33:07 +0000

    armv71 ccsrv

--- CCServer.cpp
+++ CCServer.cpp
@@ -50,10 +50,10 @@ void CCServer::_setup_socket() {
         throw;
     } 
 
-    #ifdef __aarch64__
+    #ifdef __ARM_ARCH_7A__
         LOG_F(WARNING, "ARM architecture detected");
         memset(&b_iface, 0, sizeof(b_iface));
-        snprintf(b_iface.ifr_name, sizeof(b_iface.ifr_name), b_iface_name);
+        snprintf(b_iface.ifr_name, sizeof(b_iface.ifr_name), b_iface.ifr_name);
         if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, (void*)&b_iface, sizeof(b_iface)) < 0) {
             close(sockfd);
             perror("setsockopt (SO_BINDTODEVICE)");
